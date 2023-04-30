de2cfce949bd21f02541ed8ab2c5cf6b657e7fb8
vishalauviti <130144278+vishalauviti@users.noreply.github.com>
2023-04-30 18:50:54 +0530

    removed some un used code
    removed some un used code

--- src/main/java/italent_test/utils/CSVUtil.java
+++ src/main/java/italent_test/utils/CSVUtil.java
@@ -16,14 +16,6 @@ public class CSVUtil {
         public static String TYPE = "text/csv";
         static String[] headers = { "Name", "Author","Published","Price","Description","Pages","Language","Publisher","Rating" };
 
-        public static boolean hasCSVFormat(MultipartFile file) {
-            if (TYPE.equals(file.getContentType())
-                    || file.getContentType().equals("application/vnd.ms-excel")) {
-                return true;
-            }
-            return false;
-        }
-
         public static List<Book> csvToTutorials(InputStream is) {
             try (BufferedReader fileReader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
                  CSVParser csvParser = new CSVParser(fileReader,
@@ -52,27 +44,6 @@ public class CSVUtil {
                 return bookList;
             } catch (IOException e) {
                 throw new RuntimeException("fail to parse CSV file: " + e.getMessage());
-            }
-        }
-
-        public static ByteArrayInputStream tutorialsToCSV(List<Book> bookList) {
-            final CSVFormat format = CSVFormat.DEFAULT.withQuoteMode(QuoteMode.MINIMAL);
-
-            try (ByteArrayOutputStream out = new ByteArrayOutputStream();
-                 CSVPrinter csvPrinter = new CSVPrinter(new PrintWriter(out), format);) {
-                for (Book book : bookList) {
-                    List<String> data = Arrays.asList(
-                            String.valueOf(book.getId()),
-                            book.getBookName(),
-                            book.getDescription()
-                    );
-                    csvPrinter.printRecord(data);
-                }
-
-                csvPrinter.flush();
-                return new ByteArrayInputStream(out.toByteArray());
-            } catch (IOException e) {
-                throw new RuntimeException("fail to import data to CSV file: " + e.getMessage());
             }
         }
 }
